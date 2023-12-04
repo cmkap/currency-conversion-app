36837ab1b89fc18bf4ce5615f0e08437f9546371
chisambwe.k <chisambwe.k@carbonyte.io>
2023-12-04 08:33:15 +0000

    updated api

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+// const baseURL = `https://v6.exchangerate-api.com/v6/${
+//   import.meta.env.VITE_API_KEY
+// }/latest/`;
+
 const baseURL = `https://v6.exchangerate-api.com/v6/1209896fa7f068e468cca43a/latest/`;
 
 const apiClient = axios.create({
