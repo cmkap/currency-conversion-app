c47e87e202a0a5cc43f980029e7117d5b026ddcc
chisambwe.k <chisambwe.k@carbonyte.io>
2023-12-04 08:22:39 +0000

    Updated readme and Added previous amounts

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,27 @@ import { FaExchangeAlt } from "react-icons/fa";
 import CurrencySelector from "./components/CurrencySelector";
 import useCurrency from "./hooks/useCurrency";
 
+interface Conversion {
+  fromCurrency: number;
+  fromCurrencySelected: string;
+  toCurrencySelected: string;
+  toCurrency: number;
+}
+
 function App() {
   const [fromCurrency, setFromCurrency] = useState<number>(1);
-  const [fromCurrencySelected, setFromCurrencySelected] = useState<string>("USD");
+  const [fromCurrencySelected, setFromCurrencySelected] =
+    useState<string>("USD");
   const [toCurrencySelected, setToCurrencySelected] = useState<string>("USD");
+  const [conversionHistory, setConversionHistory] = useState<Conversion[]>([]);
 
   const { data: currency } = useCurrency(fromCurrencySelected);
 
   const conversionRate = currency?.conversion_rates?.[toCurrencySelected] || 0;
-  const toCurrency = isNaN(fromCurrency) || isNaN(conversionRate)
-    ? 0
-    : fromCurrency * conversionRate;
+  const toCurrency =
+    isNaN(fromCurrency) || isNaN(conversionRate)
+      ? 0
+      : fromCurrency * conversionRate;
 
   const handleFromCurrencyChange = (e: ChangeEvent<HTMLInputElement>) => {
     setFromCurrency(parseFloat(e.target.value));
@@ -21,12 +31,22 @@ function App() {
 
   const handleFromCurrencySelect = (currency: string) => {
     setFromCurrencySelected(currency);
+    console.log("handFrom", currency);
+    setConversionHistory([
+      ...conversionHistory,
+      { fromCurrencySelected, fromCurrency, toCurrencySelected, toCurrency },
+    ]);
   };
 
   const handleToCurrencySelect = (currency: string) => {
     setToCurrencySelected(currency);
+    setConversionHistory([
+      ...conversionHistory,
+      { fromCurrencySelected, fromCurrency, toCurrencySelected, toCurrency },
+    ]);
   };
 
+  console.log(conversionHistory);
   return (
     <div
       className="d-flex flex-column align-items-center justify-content-center gap-3"
@@ -55,6 +75,15 @@ function App() {
         />
         <CurrencySelector onSelect={handleToCurrencySelect} />
       </div>
+      <h2>Converstion History</h2>
+      <ul>
+        {conversionHistory.map((conversion, index) => (
+          <li key={`${conversion}${index}`}>
+            {conversion.fromCurrency} {conversion.fromCurrencySelected} to{" "}
+            {conversion.toCurrency} {conversion.toCurrencySelected}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
